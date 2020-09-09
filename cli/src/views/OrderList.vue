b258af605c575b3e1095068c7abb47165e82cefc
kyawkhantzin <kyawkhantzinmic2020@gmail.com>
2020-09-10 00:15:32 +0630

    uploade order

--- cli/src/views/OrderList.vue
+++ cli/src/views/OrderList.vue
@@ -5,11 +5,25 @@
 		<div class="row">
 			<div v-for="currency in info" :key="currency.order_id" class="col-md-4">
 				<table class="table table-bordered bg-light">
-					<tr><td colspan="2" class="bg-danger text-warning font-weight-bold">Order List</td></tr>
-					<tr><td class="font-weight-bold text-success">Voucher No:</td><td>{{currency.order_voucherno}}</td></tr>
-					<tr><td class="font-weight-bold text-success">Total:</td><td>{{currency.order_total}}</td></tr>
-					<tr><td class="font-weight-bold text-success">User Name:</td><td>{{currency.order_user.user_name}}</td></tr>
-					<tr><td></td><td><button class="btn btn-sm btn-outline-secondary">Detail</button></td></tr>
+					<thead>
+						<tr>
+							<th>Order List</th>
+							<th>Voucher No</th>
+							<th>Total</th>
+							<th>User Name</th>
+							<th>Detail</th>
+						</tr>
+					</thead>
+
+					<tbody>
+						<tr>
+							<td>{{currency.order_voucherno}}</td>
+							<td>{{currency.order_total}}</td>
+							<td>{{currency.order_user.user_name}}</td>
+							<td></td>
+							<td></td>
+						</tr>
+					</tbody>
 
 				</table>
 			</div>
@@ -30,7 +44,7 @@
 			axios.get('http://osapi.thetpainghtut.com/api/v1/orders')
 			.then(res=>{
 				this.info=res.data.orders;
-			})
+			});
 		}
 	}
 	
